e243763b4ea040b95137a3cc1f2232976b859cfa
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2021-10-06 18:09:26 +0200

    up

--- gen.py
+++ gen.py
@@ -11,7 +11,8 @@ firstnames = ws['C']
 sujets = ws['F']
 entreprises = ws['D']
 pres = ws['R']
-
+stages=ws['M']
+phds = ws['N']
 #masterlist = (('m1'), ('m2'))
 masterlist = (('m2'))
 print(masterlist)
@@ -22,11 +23,11 @@ def writeRapports(f, n, fn, s, e, p):
 """.format(n.value.title().replace(" ", ""), n.value.title(), fn.value.title(), e.value.title().strip(), s.value.capitalize().strip(), str(p.value).strip()))
 
 
-def writeTableEntry(f, n, fn, s, e, p):
+def writeTableEntry(f, n, fn, s, e, p, stage, phd):
     f.write(
         """
-| {0} | {1} |  _{3}_ | link:{{attachmentsdir}}/presentations/{4}.pdf[{4}.pdf] 
-""".format(n.value.title(), fn.value.title(), e.value.title().strip(), s.value.strip(), str(p.value).strip()))
+| {0} | {1} |  _{3}_ | link:{{attachmentsdir}}/presentations/{4}.pdf[{4}.pdf]  | {5} | {6}
+""".format(n.value.title(), fn.value.title(), e.value.title().strip(), s.value.strip(), str(p.value).strip(), str(stage.value), str(phd.value)))
 
 
 for module in ['m2']:
@@ -38,9 +39,12 @@ for module in ['m2']:
     f.close()
     f = open("modules/"+module+"/partials/projets.adoc", "w")
     f.write('[cols="1,1,2,4"]\n|===\n')
-    f.write('| Nom | Prénom | Entreprise | Sujet\n')
-    for n, fn, s, e, p in sorted(zip(names, firstnames, sujets, entreprises, pres), key=lambda x: x[0].value):
-            writeTableEntry(f, n, fn, s, e, p)
+    f.write('| Nom | Prénom | Entreprise | Sujet | Stage | Doctorat \n')
+    for n, fn, s, e, p, stage,phd in sorted(zip(names, firstnames, sujets, entreprises, pres,stages,phds), key=lambda x: x[0].value):
+            if n.value == 'Nom':
+                continue
+            print(stage.value, ' ', phd.value )
+            writeTableEntry(f, n, fn, s, e, p,stage,phd)
     f.write('\n|===')
     f.close()
 
