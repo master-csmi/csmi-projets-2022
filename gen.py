162c3157d708966c2044d6ab5ab1d76c2e319c6d
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2021-10-06 17:26:57 +0200

    up

--- gen.py
+++ gen.py
@@ -1,56 +1,56 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
 import itertools
-wb = load_workbook('stages.xlsx',)
+wb = load_workbook('projets-m2.xlsx',)
 print('sheets:',wb.sheetnames)
 
 
-ws = wb['exportConvention']
-names=ws['C']
-firstnames = ws['D']
-codes = ws['L']
-sujets = ws['T']
-entreprises = ws['BB']
-www_entreprises = ws['BQ']
+ws = wb['Form Responses 1']
+names=ws['B']
+firstnames = ws['C']
+sujets = ws['F']
+entreprises = ws['D']
+pres = ws['R']
 
-masterlist = (('m1', 'MI6251'), ('m2', 'MI6252'))
-
-def writeRapports(f, n, fn, s, e, w ):
+#masterlist = (('m1'), ('m2'))
+masterlist = (('m2'))
+print(masterlist)
+def writeRapports(f, n, fn, s, e, p):
     f.write(
 """
- - [[[{0}]]] {1} {2}, _{4}_, link:{5}[{3}], link:{{attachmentsdir}}/++{1}-{2}.pdf++[{1}-{2}.pdf],  link:{{attachmentsdir}}/++{1}-{2}-slides.pdf++[{1}-{2}-slides.pdf] 
-""".format(n.value.title().replace(" ", ""), n.value.title(), fn.value.title(), e.value.title().strip(), s.value.capitalize().strip(), w.value.strip()))
+ - [[[{0}]]] {1} {2}, _{4}_, link:{{attachmentsdir}}/presentations/{5}.pdf[{5}.pdf] 
+""".format(n.value.title().replace(" ", ""), n.value.title(), fn.value.title(), e.value.title().strip(), s.value.capitalize().strip(), str(p.value).strip()))
 
 
-def writeTableEntry(f, n, fn, s, e, w):
+def writeTableEntry(f, n, fn, s, e, p):
     f.write(
         """
-| {0} | {1} | link:{4}[{2}] | _{3}_
-""".format(n.value.title(), fn.value.title(), e.value.title().strip(), s.value.strip(), w.value.strip()))
+| {0} | {1} |  _{3}_ | link:{{attachmentsdir}}/presentations/{4}.pdf[{4}.pdf] 
+""".format(n.value.title(), fn.value.title(), e.value.title().strip(), s.value.strip(), str(p.value).strip()))
 
 
-for module,master in masterlist:
-    f = open("modules/"+module+"/partials/rapports.adoc", "w")
-    for n, fn, c, s, e,w in sorted(zip(names, firstnames, codes, sujets, entreprises,www_entreprises), key=lambda x: x[0].value):
-        if c.value == master :
-            writeRapports(f, n, fn, s, e, w)
+for module in ['m2']:
+    print('module:',module)
+    f = open("modules/"+module+"/partials/presentations.adoc", "w")
+    for n, fn, s, e, p in sorted(zip(names, firstnames, sujets, entreprises, pres), key=lambda x: x[0].value):
+#            print(str(p.value).strip())
+            writeRapports(f, n, fn, s, e, p)
     f.close()
-    f = open("modules/"+module+"/partials/stages.adoc", "w")
+    f = open("modules/"+module+"/partials/projets.adoc", "w")
     f.write('[cols="1,1,2,4"]\n|===\n')
     f.write('| Nom | Prénom | Entreprise | Sujet\n')
-    for n, fn, c, s, e,w in sorted(zip(names, firstnames, codes, sujets, entreprises,www_entreprises), key=lambda x: x[0].value):
-        if c.value == master :
-            writeTableEntry(f, n, fn, s, e, w)
+    for n, fn, s, e, p in sorted(zip(names, firstnames, sujets, entreprises, pres), key=lambda x: x[0].value):
+            writeTableEntry(f, n, fn, s, e, p)
     f.write('\n|===')
     f.close()
 
-encadrants = ws['CA']
+encadrants = ws['E']
 
-emails={'m1':' ','m2':' '}
-for module, master in masterlist:
-    for n,c, e in zip(names,codes,encadrants):
-        if c.value == master:
+#emails={'m1':' ','m2':' '}
+emails = { 'm2': ' '}
+for module in ['m2']:
+    for n,e in zip(names,encadrants):
             emails[module]+=e.value.strip()+','
 
-print('M1:',emails['m1'])
+#print('M1:',emails['m1'])
 print('M2:',emails['m2'])
